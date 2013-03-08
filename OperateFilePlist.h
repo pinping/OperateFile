e7c78475bfac3a3d96efc12cf78a7def211056c6
pinping <377795496@163.com>
2013-03-08 11:11:28 +0800

    Create OperateFilePlist.h

---
+++ OperateFilePlist.h
@@ -0,0 +1,45 @@
+//
+//  OperateFilePlist.h
+//  MacCamera
+//
+//  Created by ZhaoPinYang on 12-12-4.
+//  Copyright (c) 2012年 maclove. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OperateFilePlist : NSObject{
+  	
+}
+
++ (NSArray *) MacLoveRepeat:(NSArray *)RepeatArr;
+
+
++ (NSString *)generateTradeNOPlist;
+
+
++ (NSString *)generateTradeNO;
+
+
++ (void)saveToFile:(NSString *) FilePath
+		  FileName:(NSString *) FileName
+		  FileType:(NSString *) FileType
+		FileImagey:(UIImage	*)FileImagey;
+
+
++ (void) SaveToFilePlist: (NSString *) FilePath
+				FileName: (NSString *) FileName
+				FileType: (NSString *) FileType
+			   FileArray: (NSArray	*) FileArray;
+
+
++ (NSArray *)loadFromFile:(NSString *) FilePath
+				 FileName:(NSString *) FileName
+				 FileType:(NSString *) FileType;
+
+
++ (NSString *) dataFilePath:(NSString *) FilePath
+				   FileName:(NSString *) FileName
+				   FileType:(NSString *) FileType;
+
+@end
